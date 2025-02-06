30b41b430b89d885fa3c5dd513016e57001da296
Devang Tyagi <devangtyagi626@gmail.com>
2025-02-06 23:30:01 +0530

    update

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,27 +1,29 @@
-import { useSignIn, useUser } from "@clerk/clerk-react";
-import { Navigate, useLocation } from "react-router-dom";
+import { useUser } from '@clerk/clerk-react'
+import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
-  const { isSignedIn, user, isLoaded } = useUser();
-  const { pathname } = useLocation();
-  const { signIn } = useSignIn();
+const ProtectedRoute = ({children}) => {
+    const {isSignedIn , user , isLoaded} = useUser();
+    const {pathname} = useLocation();
 
-  if (!isLoaded) {
-    return null;
-  }
-  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
-    signIn.redirectToSignIn({ returnBackUrl: pathname });
-    return null;
-  }
+    if (!isLoaded) {
+        return null; 
+      }
+    if(isLoaded && !isSignedIn && isSignedIn!==undefined){
+        return <Navigate to="/?sign-in==true" />
+    }
 
-  if (
-    user !== undefined &&
-    !user?.unsafeMetadata?.role &&
-    pathname !== "/onboarding"
-  )
-    return <Navigate to="/onboarding" />;
+    if (
+        user !== undefined &&
+        !user?.unsafeMetadata?.role &&
+        pathname !== "/onboarding"
+      )
+        return <Navigate to="/onboarding" />;
 
-  return children;
-};
 
-export default ProtectedRoute;
+    return children;
+}
+
+export default ProtectedRoute
+
+
+

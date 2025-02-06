0fbb74e81a91e358b4c3a420555558531e8a24ab
Devang Tyagi <devangtyagi626@gmail.com>
2025-02-06 23:15:51 +0530

    update

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,4 +1,4 @@
-import { useUser } from "@clerk/clerk-react";
+import { useSignIn, useUser } from "@clerk/clerk-react";
 import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
